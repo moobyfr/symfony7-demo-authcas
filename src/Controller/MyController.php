eac50c782302e9ce1242584d0a38590894c83d23
Emmanuel Blindauer <e.blindauer@gmail.com>
2024-08-01 17:24:29 +0300

    CAS working

---
+++ src/Controller/MyController.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MyController extends AbstractController {
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        $luckyURL=$this->generateUrl('number');
+        return new Response(
+            "<html><body><a href='$luckyURL'> Lucky Number</a></body></html>"
+        );
+    }
+
+    #[Route('/number', name: 'number')]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+        $homeURL=$this->generateUrl('home');
+        return new Response(
+            "<html><body>Lucky number: $number <br> <a href='$homeURL'>HOME</a></body></html>"
+        );
+    }
+}
+
---
+++ src/Security/CasAuthenticatorEntryPoint.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+
+class CasAuthenticatorEntryPoint implements AuthenticationEntryPointInterface
+{
+    private string $casUrl;
+
+    public function __construct(string $casUrl)
+    {
+        $this->casUrl = $casUrl;
+    }
+
+    public function start(Request $request, AuthenticationException $authException = null): \Symfony\Component\HttpFoundation\Response
+    {
+        $currentUrl = $request->getUri();
+        $redirectUrl = sprintf('%s?service=%s', $this->casUrl, urlencode($currentUrl));
+
+        return new RedirectResponse($redirectUrl);
+
+    }
+}
+
