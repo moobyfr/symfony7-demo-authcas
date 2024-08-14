c9fe3268e3fbaccf1d680ddc96e6193a973d624a
Emmanuel Blindauer <e.blindauer@gmail.com>
2024-08-14 08:23:20 +0300

    Add link to self URI in the protected route to test full URL explicit protect the /number controller

--- src/Controller/MyController.php
+++ src/Controller/MyController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -18,12 +19,14 @@ class MyController extends AbstractController
     }
 
     #[Route('/number', name: 'number')]
-    public function number(): Response
+    public function number(Request $request): Response
     {
         $number = random_int(0, 100);
         $homeURL = $this->generateUrl('home');
+        $currentUrl = $request->getUri();
+
         return new Response(
-            "<html><body>Lucky number: $number <br> <a href='$homeURL'>HOME</a></body></html>"
+            "<html><body>Lucky number: $number <br> <a href='$currentUrl'>SELF</a> <br><a href='$homeURL'>HOME</a></body></html>"
         );
     }
 }
